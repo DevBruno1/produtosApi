8cbf23f022680448eb69589f0e39241659d70b87
Bruno Bastos <brbbastos7@gmail.com>
2026-08-15 17:40:11 -0300

    first commit

---
+++ src/test/java/com/devbruno/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbruno.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
